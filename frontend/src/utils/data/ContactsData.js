bfff190cde593eb09e5bf71fbf17c8b7f8e4af62
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-09-22 15:31:37 +0530

    Update ContactsData.js

--- frontend/src/utils/data/ContactsData.js
+++ frontend/src/utils/data/ContactsData.js
@@ -1,7 +1,7 @@
 export const ContactsData = {
   email: "velpumaduguharsha@gmail.com",
   phone: "+918688344363",
-  address: "Kurnool, Andhra Pradesh",
+  address: "Kurnool, Andhra Pradesh,India",
   github: "https://github.com/HarshaVelpumadugu",
   linkedIn: "https://www.linkedin.com/in/velpumadugu-harsha-93a836236/",
   leetcode: "https://leetcode.com/u/harsha_vlc/",

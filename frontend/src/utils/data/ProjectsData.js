70938a94df29c1e29e06d49cb06d417832abf5fb
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-09-23 08:52:05 +0530

    Update ProjectsData.js

--- frontend/src/utils/data/ProjectsData.js
+++ frontend/src/utils/data/ProjectsData.js
@@ -19,7 +19,7 @@ export const ProjectsData = [
   {
     title: "Portfolio",
     description:
-      "A modern and interactive personal portfolio built with React and powered by Framer Motion for smooth, eye-catching animations. The portfolio showcases projects, skills, and experience with a clean and responsive layout. It includes a dark and light theme toggle for a personalized browsing experience, ensuring both accessibility and style."
+      "A modern and interactive personal portfolio built with React and powered by Framer Motion for smooth, eye-catching animations. The portfolio showcases projects, skills, and experience with a clean and responsive layout. It includes a dark and light theme toggle for a personalized browsing experience, ensuring both accessibility and style.",
     techStack: ["React", "Framer-Motion"],
     liveLink: "https://portfolio-6p91.onrender.com",
     githubLink: "https://github.com/HarshaVelpumadugu/Portfolio",

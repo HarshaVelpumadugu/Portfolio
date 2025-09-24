f4b1d7570c37a4a727aec16ce3e24df366526906
HarshaVelpumadugu <velpumaduguharsha@gmail.com>
2025-09-24 13:35:54 +0530

    Changed Some Styles

--- frontend/src/components/Certifications/Certifications.jsx
+++ frontend/src/components/Certifications/Certifications.jsx
@@ -8,8 +8,8 @@ const Certifications = () => {
     <section id="certifications" className="section">
       <motion.div
         className="container"
-        initial={{ opacity: 0, x: 50 }}
-        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
         viewport={{ once: true }}
       >

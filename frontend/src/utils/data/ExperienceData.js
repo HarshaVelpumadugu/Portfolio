b0f869370ff84b0ccdcd9db37c19b36b3dbfbabe
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-09-30 23:07:26 +0530

    Update ExperienceData.js

--- frontend/src/utils/data/ExperienceData.js
+++ frontend/src/utils/data/ExperienceData.js
@@ -5,7 +5,7 @@ export const ExperienceData = [
     date: "Feb 2025 - Mar 2025",
     description:
       "Gained Hands on Experience in building Full-Stack Web Applications Using MERN Stack.",
-    techStack: ["React", "Node.js", "MongoDB", "Express.js"],
+    techStack: ["React.js", "Node.js", "MongoDB", "Express.js"],
   },
   {
     title: "Junior Software Developer Intern",
@@ -13,6 +13,6 @@ export const ExperienceData = [
     date: "Jun 2025 - Sep 2025",
     description:
       "Developing new Components using Figma and ensuring Responsiveness on WebPages and also worked on API Integration.",
-    techStack: ["React", "API Integration"],
+    techStack: ["React.js", "API Integration","Vue.js"],
   },
 ];

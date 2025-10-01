06f4b8e8ccb1583e0f49291ab84fa852bbf104b4
HarshaVelpumadugu <velpumaduguharsha@gmail.com>
2025-10-01 09:26:42 +0530

    Changed Order of Experiences

--- frontend/src/utils/data/ExperienceData.js
+++ frontend/src/utils/data/ExperienceData.js
@@ -1,18 +1,18 @@
 export const ExperienceData = [
   {
-    title: "MERN Stack Developer Intern",
-    company: "EY-GDS AICTE",
-    date: "Feb 2025 - Mar 2025",
-    description:
-      "Gained Hands on Experience in building Full-Stack Web Applications Using MERN Stack.",
-    techStack: ["React.js", "Node.js", "MongoDB", "Express.js"],
-  },
-  {
     title: "Junior Software Developer Intern",
     company: "AlterSquare",
     date: "Jun 2025 - Sep 2025",
     description:
       "Developing new Components using Figma and ensuring Responsiveness on WebPages and also worked on API Integration.",
     techStack: ["React.js", "API Integration", "Vue.js"],
+  },
+  {
+    title: "MERN Stack Developer Intern",
+    company: "EY-GDS AICTE",
+    date: "Feb 2025 - Mar 2025",
+    description:
+      "Gained Hands on Experience in building Full-Stack Web Applications Using MERN Stack.",
+    techStack: ["React.js", "Node.js", "MongoDB", "Express.js"],
   },
 ];
